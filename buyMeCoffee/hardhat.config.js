69374252194e2de5d779fbf2ab08f41c3129bf4f
Arpish00 <106444339+Arpish00@users.noreply.github.com>
2022-11-17 22:40:36 +0530

    Update hardhat.config.js

--- buyMeCoffee/hardhat.config.js
+++ buyMeCoffee/hardhat.config.js
@@ -4,15 +4,15 @@ require("@nomiclabs/hardhat-waffle");
 
 require("dotenv").config()
 
-const GOERLI_URL = "https://eth-goerli.g.alchemy.com/v2/dk-8cgZFbri5wm1a91PrqPxJUlyY5Wqw";
-const PRIVATE_KEY = "248b8e5815bc34228fbd0f4fb2639a146c799255e4d5af2fa53c660c94791edd";
+const GOERLI_URL = YOUR GEORLI URL HERE;
+const PRIVATE_KEY = YOU PRIVATE KEY HERE;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/dk-8cgZFbri5wm1a91PrqPxJUlyY5Wqw",
+      url: [GOERLI_URL],
       accounts: [PRIVATE_KEY]
     }
   }
